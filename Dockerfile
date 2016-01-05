b1b4562626515a40cdabfcdcb36f74d75be241e3
bmb56 <erutuocneb@gmail.com>
2016-01-04 20:01:33 -0500

    Install librsvg2-bin and pngcrush packages

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM phusion/baseimage:0.9.16
 CMD ["/sbin/my_init"]
 
 #Install dependencies
-RUN apt-get install python cron lighttpd
+RUN apt-get install python cron lighttpd librsvg2-bin pngcrush
 
 #Set up cron job for updating weather forecast
 RUN crontab -l | { cat; echo "25,55 * * * * /www/weather-script.sh"; } | crontab -
--- server/weather-script.sh
+++ server/weather-script.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 python2 weather-script.py
 rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
 pngcrush -c 0 -ow weather-script-output.png
-cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
+cp -f weather-script-output.png /www/root/weather-script-output.png
